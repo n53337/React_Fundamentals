ed567b3c410e534fe6fa5c3a16a9dbad255c1d09
u53f <scalewithusef@gmail.com>
2022-11-06 21:36:43 +0100

    form validation

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -29,21 +29,65 @@ import * as React from "react";
 
 // ! USING HOOKS
 
+// function UsernameForm({ onSubmitUsername }) {
+//   const usernameInputRef = React.useRef();
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     onSubmitUsername(usernameInputRef.current.value);
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label htmlFor="usernameInput">Username:</label>
+//         <input id="usernameInput" type="text" ref={usernameInputRef} />
+//       </div>
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// function App() {
+//   const onSubmitUsername = (username) => alert(`You entered: ${username}`);
+//   return <UsernameForm onSubmitUsername={onSubmitUsername} />;
+// }
+
+// ! VALIDATE FORM
+
 function UsernameForm({ onSubmitUsername }) {
   const usernameInputRef = React.useRef();
+  const [err, setErr] = React.useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     onSubmitUsername(usernameInputRef.current.value);
   }
 
+  function handleChange(event) {
+    const data = event.target.value;
+    if (data === data.toLowerCase()) {
+      setErr(null);
+    } else {
+      setErr("The username must be in Lower-case 🚫");
+    }
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" ref={usernameInputRef} />
+        <input
+          id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          ref={usernameInputRef}
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={Boolean(err)} type="submit">
+        Submit
+      </button>
+      <div style={{ color: "tomato" }}>{err}</div>
     </form>
   );
 }
